99c4e256ff44ec5d89e16de769d355d79ef6577a
ClaudiaHernandez253 <claudiamontano143@gmail.com>
2019-11-17 23:34:33 -0600

    Cree un constructor vacio

--- app/src/main/java/com/example/mi_proyectofinal/Coros.java
+++ app/src/main/java/com/example/mi_proyectofinal/Coros.java
@@ -1,4 +1,13 @@
 package com.example.mi_proyectofinal;
 
 public class Coros {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
+
+    public Coros() {
+    }
+
+
 }
