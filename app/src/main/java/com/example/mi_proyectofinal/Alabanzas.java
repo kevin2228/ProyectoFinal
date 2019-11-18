127457a8fef71f38632762e2fab539912a566e80
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 22:38:31 -0600

    Adicione la clase de alabanzas

---
+++ app/src/main/java/com/example/mi_proyectofinal/Alabanzas.java
@@ -0,0 +1,17 @@
+package com.example.mi_proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alabanzas extends AppCompatActivity {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
+
+
+
+
+}
