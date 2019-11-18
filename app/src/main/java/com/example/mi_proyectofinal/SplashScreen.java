5291418cfac35bff006a2e97e6949688d0f39a1a
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 21:48:51 -0600

    Adicione la clase SplashScreen

---
+++ app/src/main/java/com/example/mi_proyectofinal/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.mi_proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, 4000);
+
+    }
+
+}
