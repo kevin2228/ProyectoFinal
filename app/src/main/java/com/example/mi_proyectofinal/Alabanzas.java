9b8d39f3391c74db2778c6e8e538f56dd9302bb0
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 22:42:16 -0600

    Hice los metodos getter and seter

--- app/src/main/java/com/example/mi_proyectofinal/Alabanzas.java
+++ app/src/main/java/com/example/mi_proyectofinal/Alabanzas.java
@@ -20,4 +20,36 @@ public class Alabanzas extends AppCompatActivity {
         this.autor = autor;
         this.letra = letra;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
 }
