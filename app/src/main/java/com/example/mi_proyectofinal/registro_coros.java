22ccf196a450d92c86ac53aa0a6a03d420ae76f1
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 22:35:57 -0600

    Adicione el metodo de listarCoros

--- app/src/main/java/com/example/mi_proyectofinal/registro_coros.java
+++ app/src/main/java/com/example/mi_proyectofinal/registro_coros.java
@@ -6,9 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 public class registro_coros extends AppCompatActivity {
     private AsyncHttpClient clientec = new AsyncHttpClient();
@@ -68,6 +78,99 @@ public class registro_coros extends AppCompatActivity {
 
             }
         });
+    }
+
+    private  void listarCoros(String respuesta){
+        final ArrayList<Coros> lista = new ArrayList<Coros>();
+        try{
+            JSONArray jsonArreglo = new JSONArray(respuesta);
+            for (int i=0; i<jsonArreglo.length(); i++){
+                Coros a = new Coros();
+                a.setId(jsonArreglo.getJSONObject(i).getInt("id_c"));
+                a.setTitulo(jsonArreglo.getJSONObject(i).getString("titulo"));
+                a.setAutor(jsonArreglo.getJSONObject(i).getString("autor"));
+                a.setLetra(jsonArreglo.getJSONObject(i).getString("letra"));
+
+                lista.add(a);
+
+            }
+
+            final ArrayAdapter<Coros> a = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, lista);
+            lvdatosc.setAdapter(a);
+
+
+            buscar.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    a.getFilter().filter(s);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+            lvdatosc.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Coros a = lista.get(position);
+                    String url = "https://proyectofinalsis21.000webhostapp.com/eliminarCoro.php?id_c="+a.getId();
+
+                    clientec.post(url, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            if (statusCode == 200){
+                                Toast.makeText(registro_coros.this, "Coro liminado Correctamente", Toast.LENGTH_SHORT).show();
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                obtenerCoros();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                        }
+                    });
+
+                    return true;
+                }
+            });
+
+            lvdatosc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Coros a = lista.get(position);
+                    StringBuffer b = new StringBuffer();
+                    b.append("ID: " + a.getId() + "\n");
+                    b.append("TITULO: " + a.getTitulo() + "\n");
+                    b.append("AUTOR: " + a.getTitulo() + "\n");
+                    b.append("LETRA: " + a.getLetra() + "\n");
+
+                    AlertDialog.Builder al = new AlertDialog.Builder(registro_coros.this);
+                    al.setCancelable(true);
+                    al.setTitle("Descripción Coro");
+                    al.setMessage(a.tostring());
+                    al.show();
+                }
+            });
+
+
+        }catch(Exception el){
+            el.printStackTrace();
+        }
+
+
     }
 
 }
