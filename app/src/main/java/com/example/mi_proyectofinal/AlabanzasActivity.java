4171be69f06df25499881070b2d5b731c7155647
lizita951 <lopezarely930@gmail.com>
2019-11-18 07:50:25 -0600

    Verificando codigo

--- app/src/main/java/com/example/mi_proyectofinal/AlabanzasActivity.java
+++ app/src/main/java/com/example/mi_proyectofinal/AlabanzasActivity.java
@@ -34,7 +34,20 @@ public class AlabanzasActivity extends AppCompatActivity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setIcon(R.drawable.ic_close)
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
             return true;
         }
         //para las demas cosas, se reenvia el evento al listener habitual
