0c80268aafc94568f4969078030a25f103ea6f7c
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 22:32:58 -0600

    Adicione el metodo onKeyDown

--- app/src/main/java/com/example/mi_proyectofinal/registro_coros.java
+++ app/src/main/java/com/example/mi_proyectofinal/registro_coros.java
@@ -1,10 +1,42 @@
 package com.example.mi_proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class registro_coros extends AppCompatActivity {
+    private AsyncHttpClient clientec = new AsyncHttpClient();
+    private ListView lvdatosc;
+
+    AlertDialog.Builder dialogo;
+    EditText buscar;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setIcon(R.drawable.ic_close)
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
