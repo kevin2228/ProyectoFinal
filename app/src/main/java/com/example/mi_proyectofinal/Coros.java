951b5790cce8ce4e7f6d3ee9680790e00edf2f37
ClaudiaHernandez253 <claudiamontano143@gmail.com>
2019-11-17 23:37:57 -0600

    Hice los metodos get y set

--- app/src/main/java/com/example/mi_proyectofinal/Coros.java
+++ app/src/main/java/com/example/mi_proyectofinal/Coros.java
@@ -15,4 +15,36 @@ public class Coros {
         this.autor = autor;
         this.letra = letra;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
 }
