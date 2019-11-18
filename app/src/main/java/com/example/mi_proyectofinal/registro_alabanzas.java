db6f7cfa1d3eb440dfbb688c4f0c3cedaa1b3c50
Eduarfer <eduar.fer.nek@gmail.com>
2019-11-18 00:45:33 -0600

    Instanciando variables

--- app/src/main/java/com/example/mi_proyectofinal/registro_alabanzas.java
+++ app/src/main/java/com/example/mi_proyectofinal/registro_alabanzas.java
@@ -46,5 +46,13 @@ public class registro_alabanzas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_alabanzas);
+
+        lvdatos = findViewById(R.id.lvDatosRa);
+
+        buscar = findViewById(R.id.buscar);
+
+        cliente = new AsyncHttpClient();
+        obtenerAlabanzas();
+
     }
 }
