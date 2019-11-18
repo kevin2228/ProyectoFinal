5b7d97c99f3fd00b0099a465576dc7af673503b3
ClaudiaHernandez253 <claudiamontano143@gmail.com>
2019-11-17 23:13:23 -0600

    Adicione el metodo onKeyDown

--- app/src/main/java/com/example/mi_proyectofinal/AlabanzasActivity.java
+++ app/src/main/java/com/example/mi_proyectofinal/AlabanzasActivity.java
@@ -1,15 +1,53 @@
 package com.example.mi_proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class AlabanzasActivity extends AppCompatActivity {
+
+    private EditText ettitulo, etautor, etletra;
+    private Button btnRegistrar;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
+    private ListView lvdatos;
+
+    AlertDialog.Builder dialogo;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setIcon(R.drawable.ic_close)
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alabanzas);
+
 
     }
 }
